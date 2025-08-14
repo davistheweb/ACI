675b3177f9c0d9e64109e1778d6b5f91f9382aee
davistheweb <josiahdave001@gmail.com>
2025-08-15 00:34:36 +0100

    added scroll to top

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Outlet } from "react-router";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
+import { ScrollToTop } from "./components/ScrollToTop";
+
 
 export const Layout: React.FC = () => {
   return (
     <div>
       <Navbar />
+      <ScrollToTop />
       <Outlet />
       <Footer />
     </div>
